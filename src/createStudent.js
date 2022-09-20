474a00812ffb19b1e8f092d2ed0e618c6bb2c889
Gabrielv0id <gabriel1998.fonseca@gmail.com>
2022-09-20 17:52:57 -0300

    exercicio 5 feito

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,13 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  const perfil = {
+    name: nome,
+    feedback: () => 'Eita pessoa boa!',
+    
+  };
+  return perfil;
+};
 
 module.exports = createStudent;
