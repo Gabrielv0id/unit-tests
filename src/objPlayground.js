5a4d52b863b00434ea85b06a7ea048bc9ed50582
Gabrielv0id <gabriel1998.fonseca@gmail.com>
2022-09-20 18:59:28 -0300

    exercicio 7 feito

--- src/objPlayground.js
+++ src/objPlayground.js
@@ -34,8 +34,26 @@
   arrayGenerator('entries', { sum: 3, mult: 2, div: 0, sub: -1 }) // [ [ 'sum', 3 ], [ 'mult', 2 ], [ 'div', 0 ], [ 'sub', -1 ] ]
 */
 
-const calculator = (number1, number2) => {};
+const calculator = (number1, number2) => {
+  const obj = {
+    sum: number1 + number2,
+    mult: number1 * number2,
+    div: Math.floor(number1 / number2),
+    sub: number1 - number2,
+  };
+  return obj;
+};
 
-const arrayGenerator = (type, object) => {};
+const arrayGenerator = (type, object) => {
+  let valor;
+  if (type === 'values') {
+      valor = Object.values(object);
+  } else if (type === 'keys') {
+    valor = Object.keys(object);
+  } else {
+    valor = Object.entries(object);
+  }
+  return valor;
+};
 
 module.exports = { calculator, arrayGenerator };
